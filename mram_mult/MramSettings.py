f939714567f1bfded8369874753903b011116552
lungyenc <lungyenc@princeton.edu>
2018-07-23 16:09:05 -0400

    Add files via upload

--- mram_mult/MramSettings.py
+++ mram_mult/MramSettings.py
@@ -10,10 +10,10 @@ import math
 
 #### DEFINE THE PARAMETERS BELOW ####
 
-RES_P_MEAN = 2100   # Mean of the resistance of MRAM cells in P state
-RES_AP_MEAN = 4700  # Mean of the resistance of MRAM cell in ap state
-RES_P_STD = 200     # Standard variation of the resistance of MRAM cells in P
-RES_AP_STD = 400    # Standard variation of the resistance of MRAM cells in AP
+RES_P_MEAN = 2115   # Mean of the resistance of MRAM cells in P state
+RES_AP_MEAN = 4545  # Mean of the resistance of MRAM cell in ap state
+RES_P_STD = 112     # Standard variation of the resistance of MRAM cells in P
+RES_AP_STD = 241    # Standard variation of the resistance of MRAM cells in AP
 
 ########## DEFINITION END ###########
 
