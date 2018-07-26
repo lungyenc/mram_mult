8fb7085f30d9c38dcce1e7aced6d2245205dbfcb
lungyenc <lungyenc@princeton.edu>
2018-07-26 16:24:29 -0500

    Add files via upload

--- mram_mult/MramMultSingleBit.py
+++ mram_mult/MramMultSingleBit.py
@@ -11,19 +11,23 @@
 import sys
 import math
 import numpy as np
-import mram_mult.MramSettings as ms
+#import mram_mult.MramSettings as ms
+import MramSettings as ms
+
 
 class MramMultSingleBit:
 
     def __init__(self, nRow=None, nCol=None):
         self.nRow = nRow    # number of rows for the MRAM array
         self.nCol = nCol    # number of cols for the MRAM array
-        self.wCon = np.zeros((self.nRow, self.nCol, 2))  # conductance array
-        self.wCon[:, :, 1] = np.random.normal(ms.cpM, ms.cpStd, 
-        (self.nRow, self.nCol))
-        self.wCon[:, :, 0] = np.random.normal(ms.capM, ms.capStd, 
-        (self.nRow, self.nCol))      
-        
+        self.wCon = np.zeros((self.nRow, self.nCol, 4))  # conductance array
+        for i in range(2,4):
+            self.wCon[:, :, i] = np.random.normal(ms.cpM, ms.cpStd, 
+            (self.nRow, self.nCol))
+        for i in range(2):
+            self.wCon[:, :, i] = np.random.normal(ms.capM, ms.capStd, 
+            (self.nRow, self.nCol))      
+
     @classmethod
     def fromW(cls, w):
         return cls(np.size(w, 0), np.size(w,1))
@@ -38,17 +42,37 @@ class MramMultSingleBit:
             raise ValueError('The dimension of the W matrix is not the same as '
             'the Mram cell size')
         if (w0 != x1):
-            raise ValueError('The dimensions of the W, X matrix dont match')
+            raise ValueError('The dimensions of the W, X matrix do not match')
+        # Matrix Mapping
+        # w mapping
+        wM = np.zeros((2*w0, w1))
+        for m in range(w0):
+            for n in range(w1):
+                if (w[m, n] == 1):
+                    wM[2*m, n] = (self.wCon[m, n, 2] + self.wCon[m, n, 0])/2
+                    wM[2*m+1, n] = (self.wCon[m, n, 2] - self.wCon[m, n, 0])/2
+                else:
+                    wM[2*m, n] = (self.wCon[m, n, 3] + self.wCon[m, n, 1])/2
+                    wM[2*m+1, n] = (-self.wCon[m, n, 3] + self.wCon[m, n, 1])/2
+        print(wM)
+        # x mapping
+        xM = np.zeros((x0, 2*x1))
+        for k in range(x0):
+            for m in range(x1):
+                if (x[k, m] == 1):
+                    xM[k, 2*m] = 1
+                    xM[k, 2*m+1] = 1
+                else:
+                    xM[k, 2*m] = 1
+                    xM[k, 2*m+1] = -1               
+        print(xM)     
         # Multiplication
-        y = np.zeros((x0, w1))
+        y = np.dot(xM, wM)
+        print(y)
+        # Perfect ADC readout
         for k in range(x0):
             for n in range(w1):
-                # Conductance summation
-                acc = 0.
-                for m in range(w0):
-                    acc += self.wCon[m, n, int((w[m, n]*x[k, m]+1)/2)]
-                # Perfect ADC readout
-                accShift = acc - self.nRow * ms.capM - 0.5*ms.md
+                accShift = y[k, n] - self.nRow * ms.capM - 0.5*ms.md
                 diff = accShift // ms.md
                 if (diff >= 0 and diff <= self.nRow-2):
                     y[k, n] = -self.nRow + 2*diff + 2
@@ -60,8 +84,8 @@ class MramMultSingleBit:
 
 ### Class Test ###
 if __name__ == '__main__':   
-    testR = 16
-    testC = 2
+    testR = 128
+    testC = 3
     testXR = 1
     testXC = testR
     test = MramMultSingleBit(testR, testC)
